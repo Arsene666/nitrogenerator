e5d797779c8cd9c7d8f5ebe89ecc69cd0f6ec33b
Arsene <110703664+Arsene666@users.noreply.github.com>
2023-01-03 00:53:56 +0100

    Add files via upload

---
+++ net.py
@@ -0,0 +1,21 @@
+import time
+import random 
+import string
+import pyautogui
+
+def nitroGenerator(amount):
+    x=0
+    while x< amount:
+        length=19
+        d="discord.gift/"
+        upper=string.ascii_uppercase
+        lower=string.ascii_lowercase
+        digits=string.digits
+        generator=upper+lower+digits
+        nGenerator="".join(random.sample(generator,length))
+        fully=d+nGenerator+"\n"
+        print(fully)
+        x=x+1
+
+print("Write the amount of codes you want generated in the functions parameter. Must be a int")
+nitroGenerator(5)
